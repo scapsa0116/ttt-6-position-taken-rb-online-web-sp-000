faa654be6e10019e5a559d0ea5a814916101b81c
Stela Capsa <stela.capsa@gmail.com>
2019-10-25 02:03:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 def position_taken?(board, index)
-   if  (board[index] == " " || (board[index] == "" || (board[index] == nil)
+   if  (board[index] == " ") || (board[index] == "") || (board[index] == nil)
      return false
-   else return true
+   else 
+     return true
    end
  end
